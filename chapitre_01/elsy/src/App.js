349aaabd97b9a4ee3ae217aedcb01c8a10be2224
Massou <massou.lionel@gmail.com>
2021-04-07 15:11:00 +0200

    Elsy 02

--- chapitre_01/elsy/src/App.js
+++ chapitre_01/elsy/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tempMin = -20;
+const tempMax = 40;
+const heartMin = 80;
+const heartMax = 180;
+const stepsMin = 0;
+const stepsMax = 50000;
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className = "container-fluid">
+         {/* Hello ! */}
+        <div className = "row">
+          <p>Heart : 80</p>
+          <p>Temperature : -20</p>
+          <p>Steps : 0</p>
+
+        </div>
+      </div>
+    );
+  }
 }
 
+{/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script> */}
 export default App;
