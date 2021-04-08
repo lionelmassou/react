f5e16bfa4d196799a9876764050645c8adb01034
Massou <massou.lionel@gmail.com>
2021-04-08 16:18:31 +0200

    essai Teams

---
+++ chapitre_01/team/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Teams from "./components/teams/Teams.js"
+
+function App() {
+  return (
+    <div>
+      <h1>Liste d'équipe de foot de France</h1>
+      {const listTeam = 
+        {name: PSG, color: blue};
+        {name: OM, color: white};
+        {name: LOSC, color: red};
+        {name: TROYES, color: green};
+
+      }
+      {/* <h3>PSG</h3>
+      <h3>OM</h3>
+      <h3>LOSC</h3>
+      <h3>TROYES</h3> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ chapitre_01/team/src/components/teams/Teams.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+class Teams extends React.Component {
+    render() {
+        return (
+            <p>{this.props.name}</p>
+        )
+    }
+}
+export default Teams
