688c42a8d52a4f256ed2ea1e94c0c7185c5f617d
Lionel <massou.lionel@gmail.com>
2021-05-04 17:29:00 +0200

    catalogue first attempt

---
+++ chapitre_06/catalog/src/components/Movie.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+class Movie extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Movie
---
+++ chapitre_06/catalog/src/components/PageNotFound .jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+class PageNotFound extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                <h2>Page not found</h2>
+            </div>
+        )
+    }
+}
+
+export default PageNotFound
